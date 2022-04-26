cdeb0f83cf98691c56045d8ca720d2dfcd801e28
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-26 21:30:04 +0800

    testing: console log to see if app runs

--- utils/getQrCode.js
+++ utils/getQrCode.js
@@ -2,7 +2,15 @@
 const puppeteer = require('puppeteer');
 
 async function getQrCode() {
-	const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+	const browser = await puppeteer.launch({
+		headless: true,
+		args: [
+			'--no-sandbox',
+			'--disable-setuid-sandbox',
+			'--disable-dev-shm-usage',
+			'--single-process',
+		],
+	});
 	const page = await browser.newPage();
 	await page.goto(`https://www.topworld.com.tw/qrlogin?id=${process.env.ID}`);
 	await page.waitForSelector("button[class='_1fbEI']");

0bec759a61b33ef9a75c890e149b260294a172e7
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-26 15:12:51 +0800

    feat: allow server runs on https

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+// for using https
+const options = {
+	key: fs.readFileSync(`./localhost-key.pem`),
+	cert: fs.readFileSync(`./localhost.pem`),
+};
+const httpsServer = https.createServer(options, app);
 
 // middleware
 app.use(express.static('public'));
@@ -17,4 +24,8 @@ app.get('/', (req, res) => {
 	res.send('You has reached my server!');
 });
 
-app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+app.post('/webhook', (req, res) => {
+	res.send('HTTP POST request sent to the webhook URL!');
+});
+
+httpsServer.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));

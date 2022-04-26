904b1259fdb697fe977125a5e702853d4e7fb7d1
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-26 17:08:40 +0800

    feat: add bot reply

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
+const https = require('https');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
 
 // middleware
 app.use(express.static('public'));
@@ -13,10 +16,56 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-	res.send('You has reached my server!');
+	res.send('You has reached my server');
 });
 
 app.post('/webhook', (req, res) => {
+	if (req.body.events[0].type === 'message') {
+		const dataString = JSON.stringify({
+			replyToken: req.body.events[0].replyToken,
+			messages: [
+				{
+					type: 'text',
+					text: 'Hello, user',
+				},
+				{
+					type: 'text',
+					text: 'May I help you?',
+				},
+			],
+		});
+
+		// Request header
+		const headers = {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + TOKEN,
+		};
+
+		// Options to pass into the request
+		const webhookOptions = {
+			hostname: 'api.line.me',
+			path: '/v2/bot/message/reply',
+			method: 'POST',
+			headers: headers,
+			body: dataString,
+		};
+
+		// Define request
+		const request = https.request(webhookOptions, res => {
+			res.on('data', d => {
+				process.stdout.write(d);
+			});
+		});
+
+		// Handle error
+		request.on('error', err => {
+			console.error(err);
+		});
+
+		// Send data
+		request.write(dataString);
+		request.end();
+	}
 	res.send('HTTP POST request sent to the webhook URL!');
 });
 

087adbcc6aac3002136e10f79f10c3a51a727bed
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-26 15:54:06 +0800

    feat: remove https settings

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-// for using https
-const options = {
-	key: fs.readFileSync(`./localhost-key.pem`),
-	cert: fs.readFileSync(`./localhost.pem`),
-};
-const httpsServer = https.createServer(options, app);
 
 // middleware
 app.use(express.static('public'));
@@ -28,4 +20,4 @@ app.post('/webhook', (req, res) => {
 	res.send('HTTP POST request sent to the webhook URL!');
 });
 
-httpsServer.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}`));

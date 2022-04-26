5578388bdab5a81f79d89143b362fa49372e5bc4
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-26 20:50:22 +0800

    testing: remove screenshot first

--- utils/getQrCode.js
+++ utils/getQrCode.js
@@ -2,7 +2,7 @@
 const puppeteer = require('puppeteer');
 
 async function getQrCode() {
-	const browser = await puppeteer.launch({ headless: false });
+	const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
 	const page = await browser.newPage();
 	await page.goto(`https://www.topworld.com.tw/qrlogin?id=${process.env.ID}`);
 	await page.waitForSelector("button[class='_1fbEI']");

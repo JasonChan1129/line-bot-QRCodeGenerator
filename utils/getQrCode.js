94470490a5a6950396ffc86a779b3337fde21ad7
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-27 11:59:41 +0800

    tesing path

--- utils/getQrCode.js
+++ utils/getQrCode.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 
 async function getQrCode() {
 	console.log('execute get qr code');
-	console.log('path', process.cwd);
+	console.log('path', process.cwd());
 	const browser = await puppeteer.launch({
 		headless: true,
 		args: ['--no-sandbox'],
